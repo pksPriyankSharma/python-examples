560365f1aa0c63a535a79a2d11a04d42af184360
pksPriyankSharma <priyank74@gmail.com>
2024-09-04 18:22:37 +0530

    committing mailing-address

---
+++ mailing-address.py
@@ -0,0 +1,9 @@
+# the code is meant for printing the mailing address as appearing on the back of Envelope
+print("Complete Name")
+print("HOuse No.")
+print("Society")
+print("localtiy")
+print("District")
+print("Pin Code", "123456")
+print("State")
+print("Country")
