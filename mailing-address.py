240f9ebf117531a84c1249af8305dfd225741252
pksPriyankSharma <priyank74@gmail.com>
2024-09-04 18:41:25 +0530

    updated mailing-address file for House spelling

--- mailing-address.py
+++ mailing-address.py
@@ -1,6 +1,6 @@
 # the code is meant for printing the mailing address as appearing on the back of Envelope
 print("Complete Name")
-print("HOuse No.")
+print("House No.")
 print("Society")
 print("localtiy")
 print("District")
