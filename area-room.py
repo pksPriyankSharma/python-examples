05d3b6721b516e2e671bffdc4820f0ea206681cf
pksPriyankSharma <priyank74@gmail.com>
2024-09-11 15:18:39 +0530

    committing on 11-Sep-24 for hello.py and area-room.py

---
+++ area-room.py
@@ -0,0 +1,5 @@
+# this script helps to calculate the area of the room, with length and width being given by the user as input
+length = int(input("please input the length in mtrs : "))
+width = int(input("please input the width in mtrs : "))
+
+print("The area of room is :", length*width, " sq mtrs")
---
+++ hello.py
@@ -0,0 +1,4 @@
+# the program is meant for asking the input for user's Name and printing while addressing him with Hello
+
+name = str(input("Please enter your Name: "))
+print("Hello ", name)
