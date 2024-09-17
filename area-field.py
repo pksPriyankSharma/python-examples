767cde4dfd8e22d2e1b267f566497ac8560e3539
pksPriyankSharma <priyank74@gmail.com>
2024-09-17 22:48:02 +0530

    adding area-field file to repo

---
+++ area-field.py
@@ -0,0 +1,5 @@
+# This program is supposed to read the input of user for length and width in feet, and print the Area in acres
+# 1 acre = 43,560 sq ft
+length = int(input("Please input the length of field in feet : "))
+width = int(input("Please input the length of field in feet : "))
+area = print("The area of the field in acre is : ", int((length*width)/43560), "acre")
